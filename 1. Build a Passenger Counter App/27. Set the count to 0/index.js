8e6294df53aa56b716994c35e5477c02f25e37b9
BenjaminSemah <benjaminsemah@gmail.com>
2023-11-01 15:31:43 +0000

    section 1 lesson 15 - 28

--- 1. Build a Passenger Counter App/27. Set the count to 0/index.js
+++ 1. Build a Passenger Counter App/27. Set the count to 0/index.js
@@ -7,17 +7,15 @@ function increment() {
     countEl.textContent = count
 }
 
-// function save() {
-//     let countStr = count + " - "
-//     saveEl.textContent += countStr
-// }
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+}
 
-// let count = 0;
+let countNum = document.getElementById("count-el")
 
-// let countNum = document.getElementById("count-el")
-
-// function clickBtn() {
-//   console.log("the button is clicked!")
-//   count++;
-//   countNum.innerHTML = count
-// }
+function clickBtn() {
+  console.log("the button is clicked!")
+  count++;
+  countNum.innerHTML = count
+}
